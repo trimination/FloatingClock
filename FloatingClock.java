9dffbcca47bb4f5ffc94e6d3e798f988422ca918
Tim <tim@no7dev.com>
2017-03-19 23:36:54 +0000

    Package Name Refactor
    Changed Package Name from clock to FloatingClock

--- FloatingClock.java
+++ FloatingClock.java
@@ -1,4 +1,4 @@
-package clock;
+package FloatingClock;
 
 import java.awt.AlphaComposite;
 import java.awt.Color;
--- FloatingClockSettings.java
+++ FloatingClockSettings.java
@@ -1,4 +1,4 @@
-package clock;
+package FloatingClock;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
